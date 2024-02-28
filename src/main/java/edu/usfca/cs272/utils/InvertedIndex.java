64a28313df7134830b3c159e5a2a01f519567065
Sophie Engle <sjengle@cs.usfca.edu>
2024-02-27 17:05:33 -0800

    Comments

--- src/main/java/edu/usfca/cs272/utils/InvertedIndex.java
+++ src/main/java/edu/usfca/cs272/utils/InvertedIndex.java
@@ -7,6 +7,13 @@ import java.util.TreeMap;
 
 public class InvertedIndex {
 
+// TODO Make both of these final
+
+	/* TODO 
+	private TreeMap<String, TreeMap<String, TreeSet<Integer>>> indexes;
+	private TreeMap<String, Integer> counts;
+	*/
+	
      private SortedMap<String, SortedMap<String, ArrayList<Integer>>> indexes;
      private SortedMap<String, Integer> counts;
 
@@ -26,14 +33,16 @@ public class InvertedIndex {
      /*
       * Method to check if a variable is null and set it to a safe value if it is
       */
-     public static <T> T checkSafeValue(T variable, T safeValue) {
+     public static <T> T checkSafeValue(T variable, T safeValue) { // TODO Remove
           if (variable == null) {
                return safeValue;
           }
           return variable;
      }
 
+     // TODO public void addIndex(String word, String location, int index) {
      public void addIndex(String stem, Path p, int index) {
+    	 // TODO map.computeIfAbsent 
           SortedMap<String, ArrayList<Integer>> table = checkSafeValue(indexes.get(stem),
                     new TreeMap<>());
           ArrayList<Integer> list = checkSafeValue(table.get(p.toString()), new ArrayList<>());
@@ -46,4 +55,7 @@ public class InvertedIndex {
           counts.put(file , count);
      }
 
+     /*
+      * TODO Try to build up the general functionality
+      */
 }
