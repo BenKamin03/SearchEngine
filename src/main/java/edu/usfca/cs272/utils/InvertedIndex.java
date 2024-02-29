18fb1d77f19f7645f391fc7eae2211ae45ff821d
BenKamin03 <88076233+BenKamin03@users.noreply.github.com>
2024-02-29 15:08:01 -0800

    Updated InvertedIndex.java

--- src/main/java/edu/usfca/cs272/utils/InvertedIndex.java
+++ src/main/java/edu/usfca/cs272/utils/InvertedIndex.java
@@ -4,51 +4,28 @@ import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.SortedMap;
 import java.util.TreeMap;
+import java.util.TreeSet;
 
 public class InvertedIndex {
-
-// TODO Make both of these final
-
-	/* TODO 
-	private TreeMap<String, TreeMap<String, TreeSet<Integer>>> indexes;
-	private TreeMap<String, Integer> counts;
-	*/
 	
-     private SortedMap<String, SortedMap<String, ArrayList<Integer>>> indexes;
-     private SortedMap<String, Integer> counts;
+     private final TreeMap<String, TreeMap<String, TreeSet<Integer>>> indexes;
+     private final TreeMap<String, Integer> counts;
 
      public InvertedIndex() {
           indexes = new TreeMap<>();
           counts = new TreeMap<>();
      }
 
-     public SortedMap<String, SortedMap<String, ArrayList<Integer>>> getIndexes() {
+     public TreeMap<String, TreeMap<String, TreeSet<Integer>>> getIndexes() {
           return indexes;
      }
 
-     public SortedMap<String, Integer> getCounts() {
+     public TreeMap<String, Integer> getCounts() {
           return counts;
      }
 
-     /*
-      * Method to check if a variable is null and set it to a safe value if it is
-      */
-     public static <T> T checkSafeValue(T variable, T safeValue) { // TODO Remove
-          if (variable == null) {
-               return safeValue;
-          }
-          return variable;
-     }
-
-     // TODO public void addIndex(String word, String location, int index) {
-     public void addIndex(String stem, Path p, int index) {
-    	 // TODO map.computeIfAbsent 
-          SortedMap<String, ArrayList<Integer>> table = checkSafeValue(indexes.get(stem),
-                    new TreeMap<>());
-          ArrayList<Integer> list = checkSafeValue(table.get(p.toString()), new ArrayList<>());
-          list.add(index);
-          table.put(p.toString(), list);
-          indexes.put(stem, table);
+     public void addIndex(String word, String location, int index) {
+          indexes.computeIfAbsent(word, k -> new TreeMap<>()).computeIfAbsent(location, k -> new TreeSet<>()).add(index);
      }
 
      public void addCount(String file, int count) {
