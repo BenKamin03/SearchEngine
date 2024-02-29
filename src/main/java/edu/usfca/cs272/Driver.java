97221d499eb1d351ee15f3c084af6536e83023c8
BenKamin03 <88076233+BenKamin03@users.noreply.github.com>
2024-02-29 15:07:40 -0800

    Updated Driver.java

--- src/main/java/edu/usfca/cs272/Driver.java
+++ src/main/java/edu/usfca/cs272/Driver.java
@@ -46,7 +46,7 @@ public class Driver {
 
 	private static void run(String[] args) {
 		ArgumentParser parser = new ArgumentParser();
-		
+
 		parser.parse(args);
 
 		Path text = parser.getPath("-text");
@@ -58,73 +58,97 @@ public class Driver {
 			indexesPath = parser.getPath("-index", Path.of("index.json"));
 
 		InvertedIndex invertedIndex = new InvertedIndex();
-		
-		/* TODO 
-		if (parser.hasFlag("-text")) {
-			this stuff here
-		}
-		
-		if -index
-			try {
+
+		try {
+			FileHandler fileHandler = new FileHandler(indexesPath, countsPath, invertedIndex);
+			if (text != null) {
+				fileHandler.fillInvertedIndex(text, invertedIndex);
+			}
+			if (indexesPath != null) {
 				JsonWriter.writeObjectHash(invertedIndex.getIndexes(), indexesPath);
 			}
-	
-		*/
-		
-		
-		if (indexesPath != null || countsPath != null) {
-			try {
-				FileHandler fileHandler = new FileHandler(text, indexesPath, countsPath, invertedIndex);
-				if (text != null) {
-					fileHandler.fillInvertedIndex();
-				}
-				fileHandler.write();
-			} catch (IOException ex) {
-				System.out.println("Missing input file.");
+			if (countsPath != null) {
+				JsonWriter.writeObject(invertedIndex.getCounts(), countsPath);
 			}
-		} else {
-			System.out.println("Missing flag: -index or -counts");
+		} catch (IOException ex) {
+			System.out.println("Missing input file.");
 		}
 	}
 }
 
 /*
-TODO
-Description	Resource	Path	Location	Type
-Javadoc: Missing comment for private declaration	Driver.java	/SearchEngine/src/main/java/edu/usfca/cs272	line 46	Java Problem
-Javadoc: Missing comment for private declaration	FileHandler.java	/SearchEngine/src/main/java/edu/usfca/cs272/utils	line 10	Java Problem
-Javadoc: Missing comment for private declaration	FileHandler.java	/SearchEngine/src/main/java/edu/usfca/cs272/utils	line 10	Java Problem
-Javadoc: Missing comment for private declaration	FileHandler.java	/SearchEngine/src/main/java/edu/usfca/cs272/utils	line 10	Java Problem
-Javadoc: Missing comment for private declaration	FileHandler.java	/SearchEngine/src/main/java/edu/usfca/cs272/utils	line 11	Java Problem
-Javadoc: Missing comment for private declaration	FileHandler.java	/SearchEngine/src/main/java/edu/usfca/cs272/utils	line 85	Java Problem
-Javadoc: Missing comment for private declaration	InvertedIndex.java	/SearchEngine/src/main/java/edu/usfca/cs272/utils	line 10	Java Problem
-Javadoc: Missing comment for private declaration	InvertedIndex.java	/SearchEngine/src/main/java/edu/usfca/cs272/utils	line 11	Java Problem
-Javadoc: Missing comment for public declaration	ArgumentParser.java	/SearchEngine/src/main/java/edu/usfca/cs272/utils	line 164	Java Problem
-Javadoc: Missing comment for public declaration	FileHandler.java	/SearchEngine/src/main/java/edu/usfca/cs272/utils	line 8	Java Problem
-Javadoc: Missing comment for public declaration	FileHandler.java	/SearchEngine/src/main/java/edu/usfca/cs272/utils	line 26	Java Problem
-Javadoc: Missing comment for public declaration	FileHandler.java	/SearchEngine/src/main/java/edu/usfca/cs272/utils	line 33	Java Problem
-Javadoc: Missing comment for public declaration	FileHandler.java	/SearchEngine/src/main/java/edu/usfca/cs272/utils	line 69	Java Problem
-Javadoc: Missing comment for public declaration	InvertedIndex.java	/SearchEngine/src/main/java/edu/usfca/cs272/utils	line 8	Java Problem
-Javadoc: Missing comment for public declaration	InvertedIndex.java	/SearchEngine/src/main/java/edu/usfca/cs272/utils	line 13	Java Problem
-Javadoc: Missing comment for public declaration	InvertedIndex.java	/SearchEngine/src/main/java/edu/usfca/cs272/utils	line 18	Java Problem
-Javadoc: Missing comment for public declaration	InvertedIndex.java	/SearchEngine/src/main/java/edu/usfca/cs272/utils	line 22	Java Problem
-Javadoc: Missing comment for public declaration	InvertedIndex.java	/SearchEngine/src/main/java/edu/usfca/cs272/utils	line 29	Java Problem
-Javadoc: Missing comment for public declaration	InvertedIndex.java	/SearchEngine/src/main/java/edu/usfca/cs272/utils	line 36	Java Problem
-Javadoc: Missing comment for public declaration	InvertedIndex.java	/SearchEngine/src/main/java/edu/usfca/cs272/utils	line 45	Java Problem
-Javadoc: Missing comment for public declaration	JsonWriter.java	/SearchEngine/src/main/java/edu/usfca/cs272/utils	line 42	Java Problem
-Javadoc: Missing comment for public declaration	JsonWriter.java	/SearchEngine/src/main/java/edu/usfca/cs272/utils	line 142	Java Problem
-Javadoc: Missing comment for public declaration	JsonWriter.java	/SearchEngine/src/main/java/edu/usfca/cs272/utils	line 216	Java Problem
-Javadoc: Missing comment for public declaration	JsonWriter.java	/SearchEngine/src/main/java/edu/usfca/cs272/utils	line 297	Java Problem
-Javadoc: Missing comment for public declaration	JsonWriter.java	/SearchEngine/src/main/java/edu/usfca/cs272/utils	line 414	Java Problem
-Javadoc: Missing tag for declared exception IOException	FileHandler.java	/SearchEngine/src/main/java/edu/usfca/cs272/utils	line 45	Java Problem
-Javadoc: Missing tag for parameter countsPath	FileHandler.java	/SearchEngine/src/main/java/edu/usfca/cs272/utils	line 19	Java Problem
-Javadoc: Missing tag for parameter hash	JsonWriter.java	/SearchEngine/src/main/java/edu/usfca/cs272/utils	line 387	Java Problem
-Javadoc: Missing tag for parameter hash	JsonWriter.java	/SearchEngine/src/main/java/edu/usfca/cs272/utils	line 438	Java Problem
-Javadoc: Missing tag for parameter indexesPath	FileHandler.java	/SearchEngine/src/main/java/edu/usfca/cs272/utils	line 19	Java Problem
-Javadoc: Missing tag for parameter invertedIndex	FileHandler.java	/SearchEngine/src/main/java/edu/usfca/cs272/utils	line 19	Java Problem
-Javadoc: Missing tag for parameter textPath	FileHandler.java	/SearchEngine/src/main/java/edu/usfca/cs272/utils	line 19	Java Problem
-Javadoc: Parameter elements is not declared	JsonWriter.java	/SearchEngine/src/main/java/edu/usfca/cs272/utils	line 379	Java Problem
-Javadoc: Parameter elements is not declared	JsonWriter.java	/SearchEngine/src/main/java/edu/usfca/cs272/utils	line 425	Java Problem
-Javadoc: Parameter hash is not declared	FileHandler.java	/SearchEngine/src/main/java/edu/usfca/cs272/utils	line 41	Java Problem
-Javadoc: Parameter parser is not declared	FileHandler.java	/SearchEngine/src/main/java/edu/usfca/cs272/utils	line 17	Java Problem
-*/
+ * TODO
+ * Description Resource Path Location Type
+ * Javadoc: Missing comment for private declaration Driver.java
+ * /SearchEngine/src/main/java/edu/usfca/cs272 line 46 Java Problem
+ * Javadoc: Missing comment for private declaration FileHandler.java
+ * /SearchEngine/src/main/java/edu/usfca/cs272/utils line 10 Java Problem
+ * Javadoc: Missing comment for private declaration FileHandler.java
+ * /SearchEngine/src/main/java/edu/usfca/cs272/utils line 10 Java Problem
+ * Javadoc: Missing comment for private declaration FileHandler.java
+ * /SearchEngine/src/main/java/edu/usfca/cs272/utils line 10 Java Problem
+ * Javadoc: Missing comment for private declaration FileHandler.java
+ * /SearchEngine/src/main/java/edu/usfca/cs272/utils line 11 Java Problem
+ * Javadoc: Missing comment for private declaration FileHandler.java
+ * /SearchEngine/src/main/java/edu/usfca/cs272/utils line 85 Java Problem
+ * Javadoc: Missing comment for private declaration InvertedIndex.java
+ * /SearchEngine/src/main/java/edu/usfca/cs272/utils line 10 Java Problem
+ * Javadoc: Missing comment for private declaration InvertedIndex.java
+ * /SearchEngine/src/main/java/edu/usfca/cs272/utils line 11 Java Problem
+ * Javadoc: Missing comment for public declaration ArgumentParser.java
+ * /SearchEngine/src/main/java/edu/usfca/cs272/utils line 164 Java Problem
+ * Javadoc: Missing comment for public declaration FileHandler.java
+ * /SearchEngine/src/main/java/edu/usfca/cs272/utils line 8 Java Problem
+ * Javadoc: Missing comment for public declaration FileHandler.java
+ * /SearchEngine/src/main/java/edu/usfca/cs272/utils line 26 Java Problem
+ * Javadoc: Missing comment for public declaration FileHandler.java
+ * /SearchEngine/src/main/java/edu/usfca/cs272/utils line 33 Java Problem
+ * Javadoc: Missing comment for public declaration FileHandler.java
+ * /SearchEngine/src/main/java/edu/usfca/cs272/utils line 69 Java Problem
+ * Javadoc: Missing comment for public declaration InvertedIndex.java
+ * /SearchEngine/src/main/java/edu/usfca/cs272/utils line 8 Java Problem
+ * Javadoc: Missing comment for public declaration InvertedIndex.java
+ * /SearchEngine/src/main/java/edu/usfca/cs272/utils line 13 Java Problem
+ * Javadoc: Missing comment for public declaration InvertedIndex.java
+ * /SearchEngine/src/main/java/edu/usfca/cs272/utils line 18 Java Problem
+ * Javadoc: Missing comment for public declaration InvertedIndex.java
+ * /SearchEngine/src/main/java/edu/usfca/cs272/utils line 22 Java Problem
+ * Javadoc: Missing comment for public declaration InvertedIndex.java
+ * /SearchEngine/src/main/java/edu/usfca/cs272/utils line 29 Java Problem
+ * Javadoc: Missing comment for public declaration InvertedIndex.java
+ * /SearchEngine/src/main/java/edu/usfca/cs272/utils line 36 Java Problem
+ * Javadoc: Missing comment for public declaration InvertedIndex.java
+ * /SearchEngine/src/main/java/edu/usfca/cs272/utils line 45 Java Problem
+ * Javadoc: Missing comment for public declaration JsonWriter.java
+ * /SearchEngine/src/main/java/edu/usfca/cs272/utils line 42 Java Problem
+ * Javadoc: Missing comment for public declaration JsonWriter.java
+ * /SearchEngine/src/main/java/edu/usfca/cs272/utils line 142 Java Problem
+ * Javadoc: Missing comment for public declaration JsonWriter.java
+ * /SearchEngine/src/main/java/edu/usfca/cs272/utils line 216 Java Problem
+ * Javadoc: Missing comment for public declaration JsonWriter.java
+ * /SearchEngine/src/main/java/edu/usfca/cs272/utils line 297 Java Problem
+ * Javadoc: Missing comment for public declaration JsonWriter.java
+ * /SearchEngine/src/main/java/edu/usfca/cs272/utils line 414 Java Problem
+ * Javadoc: Missing tag for declared exception IOException FileHandler.java
+ * /SearchEngine/src/main/java/edu/usfca/cs272/utils line 45 Java Problem
+ * Javadoc: Missing tag for parameter countsPath FileHandler.java
+ * /SearchEngine/src/main/java/edu/usfca/cs272/utils line 19 Java Problem
+ * Javadoc: Missing tag for parameter hash JsonWriter.java
+ * /SearchEngine/src/main/java/edu/usfca/cs272/utils line 387 Java Problem
+ * Javadoc: Missing tag for parameter hash JsonWriter.java
+ * /SearchEngine/src/main/java/edu/usfca/cs272/utils line 438 Java Problem
+ * Javadoc: Missing tag for parameter indexesPath FileHandler.java
+ * /SearchEngine/src/main/java/edu/usfca/cs272/utils line 19 Java Problem
+ * Javadoc: Missing tag for parameter invertedIndex FileHandler.java
+ * /SearchEngine/src/main/java/edu/usfca/cs272/utils line 19 Java Problem
+ * Javadoc: Missing tag for parameter textPath FileHandler.java
+ * /SearchEngine/src/main/java/edu/usfca/cs272/utils line 19 Java Problem
+ * Javadoc: Parameter elements is not declared JsonWriter.java
+ * /SearchEngine/src/main/java/edu/usfca/cs272/utils line 379 Java Problem
+ * Javadoc: Parameter elements is not declared JsonWriter.java
+ * /SearchEngine/src/main/java/edu/usfca/cs272/utils line 425 Java Problem
+ * Javadoc: Parameter hash is not declared FileHandler.java
+ * /SearchEngine/src/main/java/edu/usfca/cs272/utils line 41 Java Problem
+ * Javadoc: Parameter parser is not declared FileHandler.java
+ * /SearchEngine/src/main/java/edu/usfca/cs272/utils line 17 Java Problem
+ */
